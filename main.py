b733786ac2f3b7e1cedf94a0ed4b2fb1ab9ee373
CJ Sheehan <SheehanCorn@gmail.com>
2022-01-04 10:11:26 -0500

    first

---
+++ main.py
@@ -0,0 +1,91 @@
+import random
+
+# arrays & strings
+arr = [0, 1, 2, 3, 4]
+print("Max value: " + str(max(arr)))
+print("size of list: " + str(len(arr)))
+arr.append(5)
+arr.pop(0)  # uses the index
+# Note: The list's remove() method only removes the first occurrence of the specified value.
+arr.remove(2)  # uses the element as argument, not index
+print(arr)
+
+# Random
+min_val = 1
+max_val = 100
+# random.randint has the range be inclusive. So if range is 1-3, possible random
+# numbers would be 1, 2, and 3
+print("Random int within range " + str(min_val) + "-" + str(max_val) + ": " +
+      str(random.randint(min_val, max_val)))
+
+# round
+num = 4.7
+print(round(num))
+print(num // 2)  # integer division
+print(num / 2)
+
+# for loops
+# for x in range(start, stop, step):
+# use end=" " to not print a new line
+for x in range(10):
+    print(x, end=" ")
+print()
+
+for x in range(1, 10):
+    print(x, end=" ")
+print()
+
+for x in range(1, 10, 2):
+    print(x, end=" ")
+print()
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x, end=" ")
+print()
+
+for x in range(1, 10):
+    print(x % 3, end=" ")
+print()
+
+# enumerate
+lst = ["car", "boat", "plane"]
+obj = enumerate(lst)
+print(list(obj))
+
+for i, val in enumerate(lst):
+    print(i, end=" ")
+    print(val, end=" ")
+print()
+
+# swap with one line, no use of a temp
+x = 2
+y = 5
+x, y = y, x
+print("x:" + str(x))
+print("y:" + str(y) + '\n')
+
+
+# generators
+# yield will pause and store the current state of the function, and start back up from where it left off
+# so you can stop the function and then resume
+def gen(my_lst):
+    for j in my_lst:
+        yield j * 2
+
+
+# using the generator in the for loop because its an iterable
+for num in gen([1, 2, 3]):
+    num += 1
+    print(num, end=" ")
+print()
+myNums = [1, 2, 3]
+for i in gen(myNums):
+    i += 1
+
+print(myNums, end=" ")
+print()
+
+# dunder methods
+
+# iterators
